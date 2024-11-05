a8f2e20650dacd9eae8356957988f7626aadd102
firhanabdurr <firhanabdurr@gmail.com>
2024-11-05 10:05:33 +0700

    revisi pasca bimbingan bersama

--- Client/src/Data/Variables.jsx
+++ Client/src/Data/Variables.jsx
@@ -1,5 +1,5 @@
 export const serverLink = "http://127.0.0.1:1322/api/auth/";
 export const clientLink = "http://127.0.0.1:3000/";
 export const phases = ["init", "voting", "result"];
-export const isFaceRecognitionEnable = true;
+export const isFaceRecognitionEnable = false;
 //n
